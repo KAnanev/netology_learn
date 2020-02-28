567e433e4d1b510da5e6926dd4cfcac0ac720489
KAnanev <russrage@gmail.com>
2020-02-28 13:30:41 +0400

    add manager_context.py

--- manager_context.py
+++ manager_context.py
@@ -9,7 +9,7 @@ class DateTime:
         self.path = path
 
     def __enter__(self):
-        self.file = open(self.path)
+        self.file = open(self.path, 'w')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -18,7 +18,14 @@ class DateTime:
         print(f'Время выполнение скрипта: {time_end - self.enter_time}')
         self.file.close()
 
+    def write_fib(self, message):
+        self.file.write(f' {message}\n')
+
 
 if __name__ == '__main__':
-    with DateTime('recipes.txt') as file:
-        i = []
+    with DateTime('testfile') as file:
+        def fibonacci(n):
+            if n in (1, 2):
+                return 1
+            return fibonacci(n - 1) + fibonacci(n - 2)
+        file.write_fib(fibonacci(10))
