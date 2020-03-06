573530c9625ac7348e37be9f80373561c1641c68
KAnanev <russrage@gmail.com>
2020-03-06 09:36:26 +0400

    add file_format_json.py, file_format_xml.py

--- file_format_json.py
+++ file_format_json.py
@@ -14,11 +14,11 @@ description = ' '.join(list_words)
 description_list = description.split(' ')
 
 for item in description_list:
-    if len(item) >= 6:
+    if len(item) > 6:
         dict_words[item] = description_list.count(item)
 
 list_words = sorted(dict_words.items(), key=lambda item: (-item[1], item[0]))
 
 for i in range(10):
-    print(f'Слово {list_words[i][0]} используется в тексте {list_words[i][1]} раза')
+    print(f'{i + 1}. Слово {list_words[i][0]} используется в тексте {list_words[i][1]} раза')
 
