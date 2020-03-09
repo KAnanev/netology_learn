4fb3df6a698d1042e3101c7660322e349ab4fa01
KAnanev <russrage@gmail.com>
2020-03-09 18:09:14 +0400

    del api key

--- yandex/task_ya_translate.py
+++ yandex/task_ya_translate.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+API_KEY = ''
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 name_file = 'FR'
