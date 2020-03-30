70d0298658fe3946c4fb333ff1904237f2014575
KAnanev <russrage@gmail.com>
2020-03-30 12:41:26 +0400

    end task_iter.py

--- advanced_python/iterators_generators_yield/task/task_iter.py
+++ advanced_python/iterators_generators_yield/task/task_iter.py
@@ -2,9 +2,10 @@ import json
 
 
 class FindWiki:
-    def __init__(self, path):
-        self.file = open(path, "r")
-        self.data = json.load(self.file)
+    def __init__(self, path_read, path_write):
+        self.read_file = open(path_read, "r")
+        self.write_file = open(path_write, 'w', encoding='utf-8')
+        self.data = json.load(self.read_file)
         self.start = 0
         self.end = len(self.data)
 
@@ -16,9 +17,14 @@ class FindWiki:
             raise StopIteration
         data_1 = self.data[self.start]
         self.start += 1
-        return data_1
+        country = data_1['translations']['rus']['common']
+        self.write_file.write(f"{country} - https://ru.wikipedia.org/wiki/{'_'.join(country.split())}\n")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.read_file.close()
+        self.write_file.close()
 
 
 if __name__ == '__main__':
-    for find_str in FindWiki('countries.json'):
-        print(find_str)
+    for find_str in FindWiki('countries.json', 'countries_link.txt'):
+        find_str
