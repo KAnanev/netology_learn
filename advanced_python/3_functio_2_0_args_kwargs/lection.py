dda2433c1b1435b51dd0c04ef689c93eca6ab6b1
KAnanev <russrage@gmail.com>
2020-03-30 18:08:57 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Advanced_Python/import_module_package/lection.py
    #	Advanced_Python/import_module_package/task/task_text
    #	advanced_python/import_module_package/lection.py
    #	advanced_python/import_module_package/task/task_text
    #	import_module_package/lection.py
    #	import_module_package/task/task_text

---
+++ advanced_python/3_functio_2_0_args_kwargs/lection.py
@@ -0,0 +1,17 @@
+# def multuply_fabric(param):
+# #
+# #     def multiply(x):
+# #         return param * x
+# #
+# #     return multiply
+# #
+# #
+# # foo  = multuply_fabric(3)
+# # print(foo(2))
+
+# def foo(*args,**kwargs):
+#     print('args', args)
+#     print('kwargs', kwargs)
+#
+# foo(1, 2, 3, v = 5, c = 7)
+
