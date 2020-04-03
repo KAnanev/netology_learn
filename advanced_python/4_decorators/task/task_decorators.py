6b087a96e6ccd9e17d701659b88a7287b9b8c6f0
KAnanev <russrage@gmail.com>
2020-04-03 11:11:26 +0400

    add task decorator

--- advanced_python/4_decorators/task/task_decorators.py
+++ advanced_python/4_decorators/task/task_decorators.py
@@ -1,16 +1,15 @@
-from datetime import time
-
-
-def args_to_string(*args, **kwargs):
-    import json
-    return f'{json.dumps(args)}_{json.dumps(kwargs)}'
-
-
 def decor_func(old_func):
     def new_func(*args, **kwargs):
-        call_time = time()
+        import json
+        from datetime import datetime
+        call_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
         result = old_func(*args, **kwargs)
-        print(call_time)
+        with open('file.log', 'a', encoding='utf-8') as file:
+            file.write(f'Время вызова функции: {call_time}\n'
+                       f'Имя функции: {old_func.__name__}\n'
+                       f'Аргументы функции: {json.dumps(args)}_{json.dumps(kwargs)}\n'
+                       f'Возвращаемое значение: {result}\n'
+                       f'\n')
         return result
     return new_func
 
@@ -18,6 +17,7 @@ def decor_func(old_func):
 @decor_func
 def func(*args, **kwargs):
     return args, kwargs
+
 
 func(3, 4)
 
